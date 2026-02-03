e86c3fdff9382b0f44dccb3c966ecfcbbff7492c
ErikDev5 <voducphuc572@gmail.com>
2026-02-03 13:24:52 +0700

    feat(ticket): implement ticket price calculation and add tests

--- schemas/TicketOrderSchema.ts
+++ schemas/TicketOrderSchema.ts
@@ -2,13 +2,19 @@ import { z } from "zod";
 
 export const TicketOrderSchema = z.object({
   weekDay: z.string().min(1),
-  time: z.coerce.number({
-    message: "Giờ không hợp lệ"
-  }),
+  time: z.coerce
+    .number({
+      message: "Giờ phải là số"
+    })
+    .min(6, "Giờ không hợp lệ")
+    .max(24, "Giờ không hợp lệ"),
   customerType: z.string().min(1),
-  customerAge: z.coerce.number({
-    message: "Tuổi của khách hàng không hợp lệ"
-  })
+  customerAge: z.coerce
+    .number({
+      message: "Tuổi của khách hàng phải là số"
+    })
+    .min(0, "Tuổi của khách hàng không hợp lệ")
+    .max(120, "Tuổi của khách hàng không hợp lệ")
 });
 
 export type TicketFormData = z.infer<typeof TicketOrderSchema>;
